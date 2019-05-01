de70712d2130cf615697193c4a5aa4f81eb41a04
João Paulo Taylor Ienczak Zanette <jpaulotiz@gmail.com>
2019-05-01 01:32:10 -0300

    First program stub.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
+extern crate structopt;
+
+use structopt::StructOpt;
+
+mod parser;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "basic")]
+struct Opt {
+    /// Debug mode
+    #[structopt(long = "parse-only")]
+    parse_only: bool,
+}
+
+fn parse() {
+    println!(">>> Parsing...");
+    let tree = parser::parse().expect("Error parsing file");
+
+    println!("Resulting tree: {:?}", tree);
+
+    println!("Done!");
+}
+
+fn compile() {
+    println!(">>> Compiling...");
+    // TODO!
+    println!("Done!");
+}
+
+fn execute() {
+    println!(">>> Executing...");
+    // TODO!
+    println!("Done!");
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opt = Opt::from_args();
+
+    if opt.parse_only {
+        parse();
+    } else {
+        parse();
+        compile();
+        execute();
+    }
 }
---
+++ src/parser/parser.rs
@@ -0,0 +1,13 @@
+#[derive(Debug, Clone)]
+pub struct ParseError;
+
+
+#[derive(Debug)]
+pub struct ParsingTree {
+    // TODO!
+}
+
+pub fn parse() -> Result<ParsingTree, ParseError> {
+    // TODO!
+    Ok(ParsingTree{})
+}
